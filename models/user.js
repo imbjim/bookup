bf27b6c3b278bf815c8ae4de0fe6b9c5bc24158e
Bjim <imbjim1@gmail.com>
2017-05-08 18:34:32 +0200

    edit profile function added

--- models/user.js
+++ models/user.js
@@ -5,7 +5,13 @@ const userSchema = new Schema({
   name: String, //added
   city: String, //added
   username: String,
-  password: String
+  password: String,
+  email: String,// added by Imre
+  phone: String,// added by Imre
+  province: String,// added by Imre
+  country: String,// added by Imre
+  age: Number,// added by Imre
+  gender: String// added by Imre
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
