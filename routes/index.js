7bc84bae48e4fcd281b5bbe24a50fa1f3031bfcf
Eduard Trias <triaseduard@gmail.com>
2017-05-09 17:58:27 +0200

    add create new book

--- routes/index.js
+++ routes/index.js
@@ -13,82 +13,38 @@ const Book = require('../models/book');
 //   res.render('index', { title: 'Express' });
 // });
 
+
+
 router.get('/', auth.isAuthenticated, (req, res, next) => {
   let user = req.user;
   // console.log(req.user)
   res.render('index', { user: user});
+});
+
+router.get('/edit-profile', auth.isAuthenticated, (req, res, next) => {
+  let user = req.user;
+  // console.log(req.user)
+  res.render('editprofile', { user: user});
 
 });
 
-router.get('/edit-profile', auth.isAuthenticated, (req, res, next) => { //added by eduard
+router.post('/', auth.isAuthenticated, (req, res, next) => { //added by eduard
   const userInfo = {
+    name: req.body.name,
     username: req.body.username,
     email: req.body.email,
     phone: req.body.phone,
     city: req.body.city,
-    province: req.body.province,
     country: req.body.country,
     age: req.body.age,
     gender: req.body.gender
   };
   User.findByIdAndUpdate(req.user._id, userInfo, (err, user)=>{
-    res.redirect('index');
+    res.redirect('/');
   });
 });
 
 //----------------------
-
-router.get('/edit-book', auth.isAuthenticated, (req, res, next) => { //added by eduard
-  let user = req.user;
-
-  res.render('editbook', { user: user});
-});
-
-router.get('/all-books', auth.isAuthenticated, (req, res, next) => { //added by eduard
-  Book.find({}, (err, book) => {
-    if (err) {
-      next(err);
-    } else {
-      // console.log(book);
-      res.render('allbooks', { book: book });
-    }
-  });
-});
-
-router.get('/available-books', auth.isAuthenticated, (req, res, next) => { //added by eduard
-  let user = req.user;
-  res.render('availablebooks', { user: user});
-});
-
-router.get('/add-book', auth.isAuthenticated, (req, res, next) => { //added by eduard
-  Book.find({}, (err, book) => {
-    if (err) {
-      next(err);
-    } else {
-      // console.log(book);
-      res.render('addbook', { book: book });
-    }
-  });
-});
-
-router.post('/add-book', (req, res, next) => {// added by Imre
-  const bookInfo = {
-    title: req.body.title,
-    author: req.body.author,
-    cover: req.body.picture,
-    description: req.body.description,
-    genre: req.body.genre,
-    pages: req.body.pages,
-  };
-
-  const newBook = new Book(bookInfo);
-  newBook.save( (err) => {
-    if (err) {
-      next(err);
-    }
-    res.redirect('auth/allbooks');
-  });
-});
 
 
 // router.get('/private', auth.isAuthenticated, (req, res, next) => {
