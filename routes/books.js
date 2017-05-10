a006d6e8744da93d0fb973f97a94344e13cbfb48
Bjim <imbjim1@gmail.com>
2017-05-10 15:46:53 +0200

    upload images functionality for new book and edit book added

--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,12 @@
 var express = require('express');
 var router = express.Router();
 
-const auth = require('../helpers/auth')
+const auth = require('../helpers/auth');
 const User = require('../models/user');
 const Book = require('../models/book');
+
+var multer  = require('multer'); //added by Imre
+var upload = multer({ dest: 'public/uploads' });
 
 
 router.get('/add-book', auth.isAuthenticated, (req, res, next) => { //added by eduard
@@ -34,11 +37,42 @@ router.post('/newBook',  auth.isAuthenticated, (req, res, next) => {// added by 
 });
 
 
-router.get('/edit-book', auth.isAuthenticated, (req, res, next) => { //added by eduard
+router.get('/editbook/:id', auth.isAuthenticated, (req, res, next) => { //added by Imre
   let user = req.user;
+  let bookId= req.params.id;
+  Book.findById({_id: bookId}, (err, book)=>{
+    if (err) throw err;
 
-  res.render('editbook', { user: user});
+      res.render('editbook', { user: user, book : book});
+
+  });
+
 });
+
+
+router.post('/editbook', upload.single('cover'), (req, res, next) => {// added by Imre
+  var bookId = req.body.id;
+  var file;
+  if(req.file !== undefined){
+    file = req.file.filename;
+  } else {
+    file = req.body.bookPicture.split("/")[1];
+  }
+  const bookInfo = {
+    title: req.body.title,
+    author: req.body.author,
+    cover: req.body.picture,
+    description: req.body.description,
+    genre: req.body.genre,
+    pages: req.body.pages,
+    picture: 'uploads/' + file
+  };
+
+  Book.findByIdAndUpdate(bookId, bookInfo, (err, book)=>{
+    res.redirect('/');
+  });
+});
+
 
 router.get('/all-books', auth.isAuthenticated, (req, res, next) => { //added by eduard
   let user = req.user;
@@ -61,7 +95,7 @@ router.get('/available-books', auth.isAuthenticated, (req, res, next) => { //add
     } else {
       res.render('availablebooks', { user: user, book: book});
     }
-  })
+  });
 
   // res.render('availablebooks', { user: user});
 });
