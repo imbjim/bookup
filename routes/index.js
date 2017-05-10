f9fc551052daf734c3204107f96bd765a0777d19
Bjim <imbjim1@gmail.com>
2017-05-10 10:58:13 +0200

    started work on upload profile pic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
+//jshint esversion: 6
+
+
 var express = require('express');
 var router = express.Router();
 
-const auth = require('../helpers/auth')
+const auth = require('../helpers/auth');
 const User = require('../models/user');
 const Book = require('../models/book');
+const Picture = require('../models/picture');//added by Imre
+
+var multer  = require('multer'); //added by Imre
+
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -38,10 +45,35 @@ router.post('/', auth.isAuthenticated, (req, res, next) => { //added by eduard
     country: req.body.country,
     age: req.body.age,
     gender: req.body.gender
+    //picture: req.picture._id
   };
   User.findByIdAndUpdate(req.user._id, userInfo, (err, user)=>{
     res.redirect('/');
   });
+});
+
+// Route to upload from project base path, added by Imre
+var upload = multer({ dest: './public/uploads/' });
+
+router.post('/upload', upload.single('photo'), function(req, res){
+  console.log(req.file);
+
+  pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
+});
+
+//Get pictures to index.ejs
+router.get('/', function(req, res, next) {
+  Picture.find((err, pictures) => {
+    res.render('index', {pictures})
+  })
 });
 
 //----------------------
