05b0527fb6873213bfcb11910d272f3ce358ae7f
Bjim <imbjim1@gmail.com>
2017-05-12 10:02:29 +0200

    send message by clicking on name of sender in messages.ejs works

--- routes/messages.js
+++ routes/messages.js
@@ -76,11 +76,57 @@ newMessage.save( (err, message) => {
 });
 
 router.get('/sendmessagetosender/:id', auth.isAuthenticated, (req, res, next) => {
-
+    console.log(req.params.id);
+  Message
+  .findById({_id: req.params.id})
+  .populate("receiver")
+  .populate("sender")
+  .populate("book")
+  .exec((err, message)=>{
+    if (err) {
+      next (err);
+      return;
+    }
+    res.render('sendmessagetosender', { message});
+  });
 });
 
 
 router.post('/sendmessagetosender',  auth.isAuthenticated, (req, res, next) => {
+  const messageInfo = {
+    subject: req.body.subject,
+    body: req.body.body,
+    book: req.body.bookId,
+    sender: req.session.passport.user,
+    receiver: req.body.receiverId
+  };
 
+  const newMessage = new Message(messageInfo);
+
+  newMessage.save( (err, message) => {
+    if (err) {
+      next(err);
+    } else {
+          User.findByIdAndUpdate({_id: req.body.receiverId}, {$push: {messages: message._id}}, (err, user) => {
+        if (err) {next (err);
+        } else {
+          User.findByIdAndUpdate({_id: req.session.passport.user}, {$push: {messages: message._id}}, (err, user) => {
+                if (err) {next (err);
+                } else {
+                  console.log("after saving", message);
+          Book.findByIdAndUpdate({_id: req.body.bookId}, {$push: {messages: message._id}}, (err, book) => {
+                        if (err) {next (err);
+                        } else {
+                          console.log(book);
+                      res.redirect('/');
+                        }
+                      });
+                }
+              });
+        }
+
+      });
+  }
+  });
 });
 module.exports = router;
