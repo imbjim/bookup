e9880c6d78a0134e4f1610ad4635da4793d9939e
Bjim <imbjim1@gmail.com>
2017-05-12 12:48:58 +0200

    inbox and sent messages added

--- routes/messages.js
+++ routes/messages.js
@@ -11,7 +11,7 @@ const Message = require('../models/message');
 var multer  = require('multer'); //added by Imre
 var upload = multer({ dest: 'public/uploads' });
 
-router.get('/messages', auth.isAuthenticated, (req, res, next) => {
+router.get('/inbox', auth.isAuthenticated, (req, res, next) => {
   // User
   // .findOne({_id: req.session.passport.user})
   // .populate("messages")
@@ -29,7 +29,22 @@ router.get('/messages', auth.isAuthenticated, (req, res, next) => {
         next (err);
         return;
       }
-      res.render('messages', { message});
+      res.render('inbox', { message});
+    });
+
+});
+
+router.get('/sentmessages', auth.isAuthenticated, (req, res, next) => {
+    Message
+    .find({sender: req.session.passport.user})
+    .populate("book")
+    .populate("receiver")
+    .exec((err, message)=>{
+      if (err) {
+        next (err);
+        return;
+      }
+      res.render('sentmessages', { message});
     });
 
 });
@@ -118,6 +133,59 @@ router.post('/sendmessagetosender',  auth.isAuthenticated, (req, res, next) => {
                         if (err) {next (err);
                         } else {
                           console.log(book);
+                      res.redirect('/');
+                        }
+                      });
+                }
+              });
+        }
+
+      });
+  }
+  });
+});
+
+router.get('/sendmessagefromsent/:id', auth.isAuthenticated, (req, res, next) => {
+    console.log(req.params.id);
+  Message
+  .findById({_id: req.params.id})
+  .populate("receiver")
+  .populate("sender")
+  .populate("book")
+  .exec((err, message)=>{
+    if (err) {
+      next (err);
+      return;
+    }
+    res.render('sendmessagefromsent', { message});
+  });
+});
+
+
+router.post('/sendmessagefromsent',  auth.isAuthenticated, (req, res, next) => {
+  const messageInfo = {
+    subject: req.body.subject,
+    body: req.body.body,
+    book: req.body.bookId,
+    sender: req.session.passport.user,
+    receiver: req.body.receiverId
+  };
+
+  const newMessage = new Message(messageInfo);
+
+  newMessage.save( (err, message) => {
+    if (err) {
+      next(err);
+    } else {
+          User.findByIdAndUpdate({_id: req.body.receiverId}, {$push: {messages: message._id}}, (err, user) => {
+        if (err) {next (err);
+        } else {
+          User.findByIdAndUpdate({_id: req.session.passport.user}, {$push: {messages: message._id}}, (err, user) => {
+                if (err) {next (err);
+                } else {
+          Book.findByIdAndUpdate({_id: req.body.bookId}, {$push: {messages: message._id}}, (err, book) => {
+                        if (err) {next (err);
+                        } else {
                       res.redirect('/');
                         }
                       });
